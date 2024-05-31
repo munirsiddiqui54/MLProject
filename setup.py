83b194dfe07bd01f221a4813cc463397b7c6665a
munirsiddiqui54 <munir22it@student.mes.ac.in>
2024-05-31 11:52:34 +0530

    update

--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPHEN_E_DOT="-e ."
+# HYPHEN_E_DOT="-e ."
 
-def getReq(path:str)->List[str]:
-    reqList=[]
-    with open(path) as file:
-        reqList=file.read().splitlines()
-        if HYPHEN_E_DOT in reqList:
-            reqList.remove(HYPHEN_E_DOT)
-    return reqList
+# def getReq(path:str)->List[str]:
+#     reqList=[]
+#     with open(path) as file:
+#         reqList=file.read().splitlines()
+#         if HYPHEN_E_DOT in reqList:
+#             reqList.remove(HYPHEN_E_DOT)
+#     return reqList
 
 
-setup(
-     name="ML Project",
-    version="0.0.1",
-    author="Munir Siddiqui",
-    author_email="munir230204@gmail.com",
-    packages=find_packages(),
-    install_requires=[
-        'pandas',
-        'numpy',
-        'matplotlib',
-        'seaborn',
-        'xgboost',
-        'catboost',
-        'scikit-learn',
-        'dill',
-        'flask',
-        'gunicorn'
-    ],
-)
+# setup(
+#      name="ML Project",
+#     version="0.0.1",
+#     author="Munir Siddiqui",
+#     author_email="munir230204@gmail.com",
+#     packages=find_packages(),
+#     install_requires=[
+#         'pandas',
+#         'numpy',
+#         'matplotlib',
+#         'seaborn',
+#         'xgboost',
+#         'catboost',
+#         'scikit-learn',
+#         'dill',
+#         'flask',
+#         'gunicorn'
+#     ],
+# )
